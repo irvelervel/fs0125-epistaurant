2cfdc030e78ebb01f0cf70f14ac487d2f69bac3c
Stefano Casasola <ste.casasola@gmail.com>
2025-04-01 11:54:44 +0200

    Adds assets and creates carousel dinamically

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row, Carousel } from 'react-bootstrap'
+import pastasciutte from '../data/menu.json'
 
 const Home = function () {
   return (
@@ -7,6 +8,30 @@ const Home = function () {
         <Col xs={12} md={8} lg={6} className="text-center">
           <h1>Epistaurant</h1>
           <h2>Le migliori pastasciutte del web!</h2>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6} className="text-center">
+          <Carousel>
+            {pastasciutte.map((pasta) => {
+              return (
+                // ogni volta che fate un .map() in JSX dovete assegnare
+                // all'elemento ritornato una prop chiamata "key", e il suo
+                // valore deve essere UNICO
+                <Carousel.Item key={pasta.id}>
+                  <img
+                    src={pasta.image}
+                    className="w-100"
+                    alt={'immagine di ' + pasta.name}
+                  />
+                  <Carousel.Caption>
+                    <h3>{pasta.name}</h3>
+                    <p>{pasta.description}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              )
+            })}
+          </Carousel>
         </Col>
       </Row>
     </Container>
