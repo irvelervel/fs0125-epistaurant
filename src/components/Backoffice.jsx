522f8963f74e51778bb229d224b6c782fc397e8a
Stefano Casasola <ste.casasola@gmail.com>
2025-04-03 10:59:45 +0200

    Fetches correctly the reservations

--- src/components/Backoffice.jsx
+++ src/components/Backoffice.jsx
@@ -4,7 +4,7 @@
 // GET sarà terminata verranno create dinamicamente le righe della nostra lista
 
 import { Component } from 'react'
-import { Container, Row, Col, ListGroup } from 'react-bootstrap'
+import { Container, Row, Col, ListGroup, Button } from 'react-bootstrap'
 
 // spoiler: anche oggi avremo bisogno di un componente dotato di STATO
 
@@ -14,12 +14,73 @@ class Backoffice extends Component {
     // che arriverà dalla chiamata GET
   }
 
+  getReservations = () => {
+    // questa funzione si occuperà di recuperare le prenotazioni a DB tramite
+    // una fetch con metodo 'GET' e salvarle all'interno di "reservations" nello
+    // state
+    const URL = 'https://striveschool-api.herokuapp.com/api/reservation'
+    fetch(URL)
+      .then((response) => {
+        if (response.ok) {
+          // ho ottenuto una response positiva dal server!
+          return response.json()
+        } else {
+          throw new Error('Errore nella fetch')
+        }
+      })
+      .then((data) => {
+        // oggi data è un array di prenotazioni!
+        console.log('PRENOTAZIONI', data)
+        this.setState({
+          reservations: data,
+        })
+        // a questo punto, dopo il setState, render() viene reinvocato
+      })
+      .catch((err) => {
+        console.log('errore', err)
+      })
+  }
+
+  // un altro metodo di lifecycle
+  // componentDidMount è un metodo riservato per i componenti react a classe
+  componentDidMount = () => {
+    console.log('SONO COMPONENTDIDMOUNT')
+    // componentDidMount è un altro metodo di lifecycle
+    // viene invocato UNA VOLTA SOLA x lifecycle
+    // viene eseguito SUBITO DOPO la PRIMA invocazione di render()
+    this.getReservations()
+    // è un metodo PERFETTO per eseguire operazioni di RECUPERO DATI
+    // necessarie al montaggio del componente
+  }
+
+  // STEPS DEL CICLO VITA DI BACKOFFICE.JSX
+  // 1) viene inizializzato lo stato del componente, quindi reservations è []
+  // 2) viene invocato render() per la prima volta, che disegna la parte "statica" del componente
+  // 3) se c'è, viene invocato componentDidMount(), che fa la GET + setState()
+  // 4) a causa di quel setState, render() viene re-invocato!
+  // 5) la lista viene riempita con i dati appena messi nello stato
+
   render() {
+    console.log('SONO RENDER')
+    // provo a invocare getReservations nel render()
+    // il render() è il metodo che si occupa di "disegnare" l'interfaccia
+    // this.getReservations()
+    // render() è un metodo che viene AUTOMATICAMENTE invocato dal componente a classe
+    // render() è un cosiddetto "metodo di lifecycle"
+    // react RE-INVOCA render() ogni volta che c'è un cambio di STATE o un cambio di PROPS
+    // quindi NON POSSIAMO invocare qui nel render() il nostro metodo getReservations(),
+    // perchè al suo interno effettuerebbe un setState() che invocherebbe nuovamente rendere()
+
     return (
       <Container>
         <Row className="justify-content-center my-5">
           <Col xs={12} md={8} lg={6}>
             <h2 className="text-center">Prenotazioni Esistenti</h2>
+            <div className="text-center my-3">
+              <Button variant="warning" onClick={this.getReservations}>
+                AGGIORNA
+              </Button>
+            </div>
             <ListGroup>
               {this.state.reservations.map((reservationObject) => {
                 return (
