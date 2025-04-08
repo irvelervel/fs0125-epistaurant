5c681d1bbbce0fc872bcb46990da14b64e770dd8
Stefano Casasola <ste.casasola@gmail.com>
2025-04-08 11:56:57 +0200

    100% Class free!

--- src/components/Backoffice.jsx
+++ src/components/Backoffice.jsx
@@ -3,7 +3,7 @@
 // avverrà quindi un caricamento all'avvio della pagina, e quando la chiamata
 // GET sarà terminata verranno create dinamicamente le righe della nostra lista
 
-import { Component } from 'react'
+import { useState, useEffect } from 'react'
 import {
   Container,
   Row,
@@ -17,15 +17,19 @@ import {
 // spoiler: anche oggi avremo bisogno di un componente dotato di STATO
 const URL = 'https://striveschool-api.herokuapp.com/api/reservation'
 
-class Backoffice extends Component {
-  state = {
-    reservations: [], // ospiterà eventualmente l'elenco delle prenotazioni
-    // che arriverà dalla chiamata GET
-    isLoading: true,
-    isError: false,
-  }
+const Backoffice = function () {
+  // state = {
+  //   reservations: [], // ospiterà eventualmente l'elenco delle prenotazioni
+  //   // che arriverà dalla chiamata GET
+  //   isLoading: true,
+  //   isError: false,
+  // }
 
-  getReservations = () => {
+  const [reservations, setReservations] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+
+  const getReservations = () => {
     // questa funzione si occuperà di recuperare le prenotazioni a DB tramite
     // una fetch con metodo 'GET' e salvarle all'interno di "reservations" nello
     // state
@@ -41,35 +45,44 @@ class Backoffice extends Component {
       .then((data) => {
         // oggi data è un array di prenotazioni!
         console.log('PRENOTAZIONI', data)
-        this.setState({
-          reservations: data,
-          // nascondo lo spinner
-          isLoading: false,
-        })
+        // this.setState({
+        //   reservations: data,
+        //   // nascondo lo spinner
+        //   isLoading: false,
+        // })
+        setReservations(data)
+        setIsLoading(false)
         // a questo punto, dopo il setState, render() viene reinvocato
       })
       .catch((err) => {
         console.log('errore', err)
-        this.setState({
-          isLoading: false,
-          isError: true,
-        })
+        // this.setState({
+        //   isLoading: false,
+        //   isError: true,
+        // })
+        setIsLoading(false)
+        setIsError(true)
       })
   }
 
   // un altro metodo di lifecycle
   // componentDidMount è un metodo riservato per i componenti react a classe
-  componentDidMount = () => {
-    console.log('SONO COMPONENTDIDMOUNT')
-    // componentDidMount è un altro metodo di lifecycle
-    // viene invocato UNA VOLTA SOLA x lifecycle
-    // viene eseguito SUBITO DOPO la PRIMA invocazione di render()
-    this.getReservations()
-    // è un metodo PERFETTO per eseguire operazioni di RECUPERO DATI
-    // necessarie al montaggio del componente
-  }
+  // componentDidMount = () => {
+  //   console.log('SONO COMPONENTDIDMOUNT')
+  //   // componentDidMount è un altro metodo di lifecycle
+  //   // viene invocato UNA VOLTA SOLA x lifecycle
+  //   // viene eseguito SUBITO DOPO la PRIMA invocazione di render()
+  //   this.getReservations()
+  //   // è un metodo PERFETTO per eseguire operazioni di RECUPERO DATI
+  //   // necessarie al montaggio del componente
+  // }
 
-  deleteReservation = (idToDelete) => {
+  useEffect(() => {
+    console.log('SONO COMPONENTDIDMOUNT')
+    getReservations()
+  }, [])
+
+  const deleteReservation = (idToDelete) => {
     fetch(URL + '/' + idToDelete, {
       method: 'DELETE',
     })
@@ -77,7 +90,7 @@ class Backoffice extends Component {
         if (response.ok) {
           // evviva!
           alert('elemento eliminato')
-          this.getReservations() // aggiorno il DOM
+          getReservations() // aggiorno il DOM
         } else {
           throw new Error('elemento non eliminato')
         }
@@ -94,85 +107,84 @@ class Backoffice extends Component {
   // 4) a causa di quel setState, render() viene re-invocato!
   // 5) la lista viene riempita con i dati appena messi nello stato
 
-  render() {
-    console.log('SONO RENDER')
-    // provo a invocare getReservations nel render()
-    // il render() è il metodo che si occupa di "disegnare" l'interfaccia
-    // this.getReservations()
-    // render() è un metodo che viene AUTOMATICAMENTE invocato dal componente a classe
-    // render() è un cosiddetto "metodo di lifecycle"
-    // react RE-INVOCA render() ogni volta che c'è un cambio di STATE o un cambio di PROPS
-    // quindi NON POSSIAMO invocare qui nel render() il nostro metodo getReservations(),
-    // perchè al suo interno effettuerebbe un setState() che invocherebbe nuovamente rendere()
+  console.log('SONO RENDER')
+  // provo a invocare getReservations nel render()
+  // il render() è il metodo che si occupa di "disegnare" l'interfaccia
+  // this.getReservations()
+  // render() è un metodo che viene AUTOMATICAMENTE invocato dal componente a classe
+  // render() è un cosiddetto "metodo di lifecycle"
+  // react RE-INVOCA render() ogni volta che c'è un cambio di STATE o un cambio di PROPS
+  // quindi NON POSSIAMO invocare qui nel render() il nostro metodo getReservations(),
+  // perchè al suo interno effettuerebbe un setState() che invocherebbe nuovamente rendere()
 
-    return (
-      <Container>
-        <Row className="justify-content-center my-5">
-          <Col xs={12} md={8} lg={6}>
-            <h2 className="text-center">Prenotazioni Esistenti</h2>
+  return (
+    <Container>
+      <Row className="justify-content-center my-5">
+        <Col xs={12} md={8} lg={6}>
+          <h2 className="text-center">Prenotazioni Esistenti</h2>
+          <div className="text-center my-3">
+            <Button
+              variant="warning"
+              onClick={() => {
+                // this.setState({ isLoading: true })
+                setIsLoading(true)
+                getReservations()
+              }}
+            >
+              AGGIORNA
+            </Button>
+          </div>
+
+          {isLoading && (
             <div className="text-center my-3">
-              <Button
-                variant="warning"
-                onClick={() => {
-                  this.setState({ isLoading: true })
-                  this.getReservations()
-                }}
-              >
-                AGGIORNA
-              </Button>
+              <Spinner variant="primary" animation="border" />
             </div>
+          )}
 
-            {this.state.isLoading && (
-              <div className="text-center my-3">
-                <Spinner variant="primary" animation="border" />
-              </div>
-            )}
+          {isError && (
+            <Alert variant="danger" className="text-center">
+              <i className="bi bi-patch-exclamation-fill me-2"></i>
+              Errore nel recupero dati
+              <i className="bi bi-patch-exclamation-fill ms-2"></i>
+            </Alert>
+          )}
 
-            {this.state.isError && (
-              <Alert variant="danger" className="text-center">
-                <i className="bi bi-patch-exclamation-fill me-2"></i>
-                Errore nel recupero dati
-                <i className="bi bi-patch-exclamation-fill ms-2"></i>
-              </Alert>
-            )}
+          <ListGroup>
+            {reservations.length === 0 && // niente prenotazioni
+              !isLoading && ( // caricamento finito
+                <ListGroup.Item>
+                  <div className="text-center">
+                    Nessuna prenotazione salvata!
+                  </div>
+                </ListGroup.Item>
+              )}
 
-            <ListGroup>
-              {this.state.reservations.length === 0 && // niente prenotazioni
-                !this.state.isLoading && ( // caricamento finito
-                  <ListGroup.Item>
-                    <div className="text-center">
-                      Nessuna prenotazione salvata!
-                    </div>
-                  </ListGroup.Item>
-                )}
-
-              {this.state.reservations.map((reservationObject) => {
-                return (
-                  <ListGroup.Item
-                    key={reservationObject._id}
-                    className="d-flex justify-content-between"
+            {reservations.map((reservationObject) => {
+              return (
+                <ListGroup.Item
+                  key={reservationObject._id}
+                  className="d-flex justify-content-between"
+                >
+                  <div>
+                    {reservationObject.name} per{' '}
+                    {reservationObject.numberOfPeople}
+                  </div>
+                  <Button
+                    variant="danger"
+                    onClick={() => {
+                      deleteReservation(reservationObject._id)
+                    }}
                   >
-                    <div>
-                      {reservationObject.name} per{' '}
-                      {reservationObject.numberOfPeople}
-                    </div>
-                    <Button
-                      variant="danger"
-                      onClick={() => {
-                        this.deleteReservation(reservationObject._id)
-                      }}
-                    >
-                      <i className="bi bi-trash-fill"></i>
-                    </Button>
-                  </ListGroup.Item>
-                )
-              })}
-            </ListGroup>
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+                    <i className="bi bi-trash-fill"></i>
+                  </Button>
+                </ListGroup.Item>
+              )
+            })}
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default Backoffice
--- src/components/ReservationForm.jsx
+++ src/components/ReservationForm.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import { useState } from 'react'
 import { Alert, Col, Container, Row } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
@@ -12,23 +12,32 @@ import Form from 'react-bootstrap/Form'
 // - dateTime <-- data e ora della prenotazione, stringa
 // - specialRequests <-- ho un cane, mi serve un seggiolone, allergie etc. etc. stringa
 
-class ReservationForm extends Component {
+const ReservationForm = function () {
   // in React, TUTTI i form che scriverete dovranno essere "CONTROLLATI" (controlled inputs)
   // un input controllato è un input che si interfaccia in real-time con lo STATO del COMPONENTE
   // abbiamo quindi bisogno di uno STATO nel componente, che memorizzerà continuamente
   // i valori inseriti nei campi del form!
 
-  state = {
-    // STATO INIZIALE DEL COMPONENTE
-    reservation: {
-      name: '',
-      phone: '',
-      numberOfPeople: '1',
-      dateTime: '',
-      smoking: false,
-      specialRequests: '',
-    },
-  }
+  // state = {
+  //   // STATO INIZIALE DEL COMPONENTE
+  //   reservation: {
+  //     name: '',
+  //     phone: '',
+  //     numberOfPeople: '1',
+  //     dateTime: '',
+  //     smoking: false,
+  //     specialRequests: '',
+  //   },
+  // }
+
+  const [reservation, setReservation] = useState({
+    name: '',
+    phone: '',
+    numberOfPeople: '1',
+    dateTime: '',
+    smoking: false,
+    specialRequests: '',
+  })
 
   // non si può fare -> this.state.reservation.specialRequests = "qualcosaltro"
   // perchè lo stato è in SOLA LETTURA (non si può modificare direttamente)
@@ -38,7 +47,7 @@ class ReservationForm extends Component {
 
   // ora che tutti i campi sono collegati a "doppia via" con lo stato,
   // posso occuparmi della logica di submit del form
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     // handlers vari su clicks etc. vanno creati con le funzioni freccia
     // per evitare uno scorretto binding del "this"
     e.preventDefault() // stoppiamo il comportamento di default del form
@@ -46,7 +55,7 @@ class ReservationForm extends Component {
     // faccio la fetch, con metodo POST
     fetch('https://striveschool-api.herokuapp.com/api/reservation', {
       method: 'POST',
-      body: JSON.stringify(this.state.reservation),
+      body: JSON.stringify(reservation),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -56,15 +65,24 @@ class ReservationForm extends Component {
           alert('PRENOTAZIONE SALVATA!')
           // sarebbe il caso di svuotare il form...
           // per farlo ripristino lo state alla forma iniziale
-          this.setState({
-            reservation: {
-              name: '',
-              phone: '',
-              numberOfPeople: '1',
-              dateTime: '',
-              smoking: false,
-              specialRequests: '',
-            },
+          // this.setState({
+          //   reservation: {
+          //     name: '',
+          //     phone: '',
+          //     numberOfPeople: '1',
+          //     dateTime: '',
+          //     smoking: false,
+          //     specialRequests: '',
+          //   },
+          // })
+
+          setReservation({
+            name: '',
+            phone: '',
+            numberOfPeople: '1',
+            dateTime: '',
+            smoking: false,
+            specialRequests: '',
           })
         } else {
           throw new Error('La chiamata non ha restituito esito positivo')
@@ -78,170 +96,191 @@ class ReservationForm extends Component {
       })
   }
 
-  render() {
-    return (
-      <Container>
-        <Row className="justify-content-center">
-          <Col xs={12} md={8} lg={6}>
-            <h2 className="my-3 text-center">Prenota il tuo tavolo ORA!</h2>
-            <Form className="mb-5" onSubmit={this.handleSubmit}>
-              <Form.Group className="mb-3">
-                <Form.Label>Il tuo nome</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Mario Rossi"
-                  required
-                  // colleghiamo il campo nome allo stato del componente
-                  // abbiamo bisogno di fare ENTRAMBE le "frecce"
-                  //   freccia n.1: collego il valore dell'input al valore dello stato
-                  value={this.state.reservation.name}
-                  //   freccia n.2: collego la modifica dell'input alla modifica dello stato
-                  onChange={(e) => {
-                    this.setState({
-                      reservation: {
-                        ...this.state.reservation,
-                        // questa riga fa in modo che il mio NUOVO oggetto reservation
-                        // non parta da oggetto vuoto, ma riparta dall'attuale
-                        // contenuto dello state! Sopra questo contenuto esistente,
-                        // vado ad aggiungere l'attuale contenuto di quello che ho scritto
-                        // nel campo in cui l'ho scritto
-                        name: e.target.value, // la lettera che ho scritto
-                        // poichè in un oggetto non posso avere due volte la stess proprietà,
-                        // di fatto vado a sovrascrivere name
-                      },
-                    })
-                  }}
-                />
-              </Form.Group>
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6}>
+          <h2 className="my-3 text-center">Prenota il tuo tavolo ORA!</h2>
+          <Form className="mb-5" onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
+              <Form.Label>Il tuo nome</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Mario Rossi"
+                required
+                // colleghiamo il campo nome allo stato del componente
+                // abbiamo bisogno di fare ENTRAMBE le "frecce"
+                //   freccia n.1: collego il valore dell'input al valore dello stato
+                value={reservation.name}
+                //   freccia n.2: collego la modifica dell'input alla modifica dello stato
+                onChange={(e) => {
+                  // this.setState({
+                  //   reservation: {
+                  //     ...this.state.reservation,
+                  //     // questa riga fa in modo che il mio NUOVO oggetto reservation
+                  //     // non parta da oggetto vuoto, ma riparta dall'attuale
+                  //     // contenuto dello state! Sopra questo contenuto esistente,
+                  //     // vado ad aggiungere l'attuale contenuto di quello che ho scritto
+                  //     // nel campo in cui l'ho scritto
+                  //     name: e.target.value, // la lettera che ho scritto
+                  //     // poichè in un oggetto non posso avere due volte la stess proprietà,
+                  //     // di fatto vado a sovrascrivere name
+                  //   },
+                  // })
 
-              {/* RENDERING CONDIZIONALE */}
-              {/* metodo dello "SHORT CIRCUIT" */}
-              {this.state.reservation.name === 'Al Bano' && (
-                <Alert variant="info">
-                  Un bicchiere di vino con un panino!
-                </Alert>
-              )}
+                  setReservation({
+                    ...reservation,
+                    name: e.target.value,
+                  })
+                }}
+              />
+            </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label>N. di telefono</Form.Label>
-                <Form.Control
-                  type="tel"
-                  required
-                  value={this.state.reservation.phone}
-                  onChange={(e) => {
-                    this.setState({
-                      reservation: {
-                        ...this.state.reservation,
-                        phone: e.target.value,
-                      },
-                    })
-                  }}
-                />
-              </Form.Group>
+            {/* RENDERING CONDIZIONALE */}
+            {/* metodo dello "SHORT CIRCUIT" */}
+            {reservation.name === 'Al Bano' && (
+              <Alert variant="info">Un bicchiere di vino con un panino!</Alert>
+            )}
 
-              <Form.Group className="mb-3">
-                <Form.Label>Quanti siete?</Form.Label>
-                <Form.Select
-                  aria-label="numero di persone"
-                  value={this.state.reservation.numberOfPeople}
-                  onChange={(e) => {
-                    this.setState({
-                      reservation: {
-                        ...this.state.reservation,
-                        numberOfPeople: e.target.value,
-                      },
-                    })
-                  }}
-                >
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                  <option>6</option>
-                  <option>7</option>
-                  <option>8</option>
-                </Form.Select>
-              </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>N. di telefono</Form.Label>
+              <Form.Control
+                type="tel"
+                required
+                value={reservation.phone}
+                onChange={(e) => {
+                  // this.setState({
+                  //   reservation: {
+                  //     ...this.state.reservation,
+                  //     phone: e.target.value,
+                  //   },
+                  // })
+                  setReservation({
+                    ...reservation,
+                    phone: e.target.value,
+                  })
+                }}
+              />
+            </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label>Quando venite?</Form.Label>
-                <Form.Control
-                  type="datetime-local"
-                  required
-                  value={this.state.reservation.dateTime}
-                  onChange={(e) => {
-                    this.setState({
-                      reservation: {
-                        ...this.state.reservation,
-                        dateTime: e.target.value,
-                      },
-                    })
-                  }}
-                />
-              </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Quanti siete?</Form.Label>
+              <Form.Select
+                aria-label="numero di persone"
+                value={reservation.numberOfPeople}
+                onChange={(e) => {
+                  // this.setState({
+                  //   reservation: {
+                  //     ...this.state.reservation,
+                  //     numberOfPeople: e.target.value,
+                  //   },
+                  // })
+                  setReservation({
+                    ...reservation,
+                    numberOfPeople: e.target.value,
+                  })
+                }}
+              >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+              </Form.Select>
+            </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Check
-                  type="checkbox"
-                  label="Tavolo fumatori?"
-                  // la proprietà "value" dei campi checkbox NON torna true/false
-                  // ma "on"/"off" (come stringhe)
-                  // poichè le API si aspettano un valore booleano per la proprietà
-                  // "smoking" e quindi devo interagire con il valore di "checked"
-                  // a livello di interfaccia
-                  checked={this.state.reservation.smoking}
-                  onChange={(e) =>
-                    this.setState({
-                      reservation: {
-                        ...this.state.reservation,
-                        smoking: e.target.checked,
-                      },
-                    })
-                  }
-                />
-              </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Quando venite?</Form.Label>
+              <Form.Control
+                type="datetime-local"
+                required
+                value={reservation.dateTime}
+                onChange={(e) => {
+                  // this.setState({
+                  //   reservation: {
+                  //     ...this.state.reservation,
+                  //     dateTime: e.target.value,
+                  //   },
+                  // })
+                  setReservation({
+                    ...reservation,
+                    dateTime: e.target.value,
+                  })
+                }}
+              />
+            </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label>Allergie, malanni, bambini?</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  value={this.state.reservation.specialRequests}
-                  onChange={(e) => {
-                    this.setState({
-                      reservation: {
-                        // dovrei in questo nuovo reservation portarmi dietro
-                        // anche tutte le altre proprietà che non ho toccato!
-                        // ovvero:
+            <Form.Group className="mb-3">
+              <Form.Check
+                type="checkbox"
+                label="Tavolo fumatori?"
+                // la proprietà "value" dei campi checkbox NON torna true/false
+                // ma "on"/"off" (come stringhe)
+                // poichè le API si aspettano un valore booleano per la proprietà
+                // "smoking" e quindi devo interagire con il valore di "checked"
+                // a livello di interfaccia
+                checked={reservation.smoking}
+                onChange={(e) =>
+                  // this.setState({
+                  //   reservation: {
+                  //     ...this.state.reservation,
+                  //     smoking: e.target.checked,
+                  //   },
+                  // })
+                  setReservation({
+                    ...reservation,
+                    smoking: e.target.checked,
+                  })
+                }
+              />
+            </Form.Group>
 
-                        // name: this.state.reservation.name,
-                        // phone: this.state.reservation.phone,
-                        // smoking: this.state.reservation.smoking,
-                        // dateTime: this.state.reservation.dateTime,
-                        // numberOfPeople: this.state.reservation.numberOfPeople,
-                        // specialeRequests: this.state.reservation.specialRequests
+            <Form.Group className="mb-3">
+              <Form.Label>Allergie, malanni, bambini?</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                value={reservation.specialRequests}
+                onChange={(e) => {
+                  // this.setState({
+                  //   reservation: {
+                  //     // dovrei in questo nuovo reservation portarmi dietro
+                  //     // anche tutte le altre proprietà che non ho toccato!
+                  //     // ovvero:
 
-                        // ma posso farlo in velocità utilizzando lo spread operator,
-                        // che crea nel mio nuovo oggetto "guscio" una copia
-                        // di tutti gli attuali valori di this.state.reservation
-                        ...this.state.reservation,
-                        specialRequests: e.target.value,
-                      },
-                    })
-                  }}
-                />
-              </Form.Group>
+                  //     // name: this.state.reservation.name,
+                  //     // phone: this.state.reservation.phone,
+                  //     // smoking: this.state.reservation.smoking,
+                  //     // dateTime: this.state.reservation.dateTime,
+                  //     // numberOfPeople: this.state.reservation.numberOfPeople,
+                  //     // specialeRequests: this.state.reservation.specialRequests
 
-              <Button variant="success" type="submit">
-                Prenota!
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+                  //     // ma posso farlo in velocità utilizzando lo spread operator,
+                  //     // che crea nel mio nuovo oggetto "guscio" una copia
+                  //     // di tutti gli attuali valori di this.state.reservation
+                  //     ...this.state.reservation,
+                  //     specialRequests: e.target.value,
+                  //   },
+                  // })
+                  setReservation({
+                    ...reservation,
+                    specialRequests: e.target.value,
+                  })
+                }}
+              />
+            </Form.Group>
+
+            <Button variant="success" type="submit">
+              Prenota!
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default ReservationForm
