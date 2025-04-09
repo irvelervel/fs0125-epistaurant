49f232bbbbede14cfcc68c621d6aa93fb412275f
Stefano Casasola <ste.casasola@gmail.com>
2025-04-09 12:54:56 +0200

    Finishes Epistaurant!

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,12 +1,13 @@
-import { Col, Container, Row, Card, Badge } from 'react-bootstrap'
-import { Link, useParams } from 'react-router-dom'
+import { Col, Container, Row, Card, Badge, Spinner } from 'react-bootstrap'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 import pastasciutte from '../data/menu.json'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const Details = function () {
   const params = useParams()
   // params è un oggetto contenente TUTTI i parametri della rotta colpita con i rispettivi valori
   console.log('PARAMS', params)
+  const navigate = useNavigate()
   // params.pastaId sarà il valore dell'id della pasta da caricare!
   // potremmo utilizzare nel JSX params.pastaId direttamente come indice dell'array!
   // ...però rendiamo fragile il nostro componente in quanto un utente esperto (e malintenzionato)
@@ -17,29 +18,56 @@ const Details = function () {
   // della rotta e verificare che sia un indice valido per l'array pastasciutte. Dopodiché
   // potremo utilizzarlo per mostrare i dettagli richiesti, o portare l'utente alla pagina '*'
   // quando non viene trovata una corrispondenza
+  const [details, setDetails] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
 
-  //   "componentDidMount"
+  // "componentDidMount"
   useEffect(() => {
     // verificheremo qui la validità del parametro "pastaId"
+    const foundPasta = pastasciutte.find((pasta) => {
+      return pasta.id.toString() === params.pastaId
+    })
+    // foundPasta sarà la pasta da far vedere nella Card! ...oppure sarà undefined
+    console.log('FOUNDPASTA', foundPasta)
+    // gestiamo il caso di errore
+    if (!foundPasta) {
+      // qui vuol dire che il find non ha trovato una corrispondenza
+      // e a ritornato foundPasta = undefined
+      // riportiamo l'utente alla 404
+      //   per Chiara
+      setIsLoading(false)
+      navigate('/404')
+    } else {
+      // qui vuol dire che la pasta è stata trovata!
+      // salviamo la pasta nello stato e spegniamo lo spinner
+      setDetails(foundPasta)
+      setIsLoading(false)
+    }
   }, [])
 
   return (
     <Container>
       <Row className="justify-content-center">
-        <Col xs={12} md={8}>
-          <Card key={pastasciutte[0].id} className="text-center my-3">
-            <Card.Img variant="top" src={pastasciutte[0].image} />
-            <Card.Body>
-              <Card.Title>{pastasciutte[0].name}</Card.Title>
-              <Card.Text>{pastasciutte[0].description}</Card.Text>
-              <Card.Text>
-                <Badge bg="info fs-6">{pastasciutte[0].price}€</Badge>
-              </Card.Text>
-              <Link to="/menu" className="btn btn-warning">
-                Torna al menu!
-              </Link>
-            </Card.Body>
-          </Card>
+        <Col xs={12} md={8} className="text-center">
+          {/* isLoading è la condizione di un ternary operator */}
+          {/* se isLoading è true, mostra lo Spinner; se è false, mostra la Card */}
+          {isLoading ? (
+            <Spinner variant="success" animation="border" />
+          ) : (
+            <Card className="text-center my-3">
+              <Card.Img variant="top" src={details.image} />
+              <Card.Body>
+                <Card.Title>{details.name}</Card.Title>
+                <Card.Text>{details.description}</Card.Text>
+                <Card.Text>
+                  <Badge bg="info fs-6">{details.price}€</Badge>
+                </Card.Text>
+                <Link to="/menu" className="btn btn-warning">
+                  Torna al menu!
+                </Link>
+              </Card.Body>
+            </Card>
+          )}
         </Col>
       </Row>
     </Container>
